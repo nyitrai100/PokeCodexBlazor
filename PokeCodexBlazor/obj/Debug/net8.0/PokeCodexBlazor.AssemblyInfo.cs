fd63d4a680cd2a371ada002acead39e69585569f
nyitraibence <nyitrai100@gmail.com>
2024-06-12 13:23:00 +0100

    Search working

--- PokeCodexBlazor/obj/Debug/net8.0/PokeCodexBlazor.AssemblyInfo.cs
+++ PokeCodexBlazor/obj/Debug/net8.0/PokeCodexBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeCodexBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3e9be5f07a6f06b1a5150c0b7917a8f58edc13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28295b5f4c1ead0b27d47da42995e326be4a89dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeCodexBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeCodexBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
