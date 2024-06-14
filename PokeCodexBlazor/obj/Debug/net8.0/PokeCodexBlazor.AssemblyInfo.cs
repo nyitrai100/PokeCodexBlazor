c2dfb86c52e1b92393bfaec798fae56f5eb1e8b6
nyitraibence <nyitrai100@gmail.com>
2024-06-14 08:51:33 +0100

    display pokemon in the first box with switch-case

--- PokeCodexBlazor/obj/Debug/net8.0/PokeCodexBlazor.AssemblyInfo.cs
+++ PokeCodexBlazor/obj/Debug/net8.0/PokeCodexBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeCodexBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9bc7dd68d4004eb6b8113b28a977e392e9a74c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224af62e7d323c7b751eb83d12a1e51690a98f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeCodexBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeCodexBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
